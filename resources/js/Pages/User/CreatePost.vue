40b9aca2ffd32f0773be41d926f6779e44e568a7
Николай <149511212+nikolayyyyyyyyy@users.noreply.github.com>
2026-01-23 23:34:40 +0200

    add logic

--- resources/js/Pages/User/CreatePost.vue
+++ resources/js/Pages/User/CreatePost.vue
@@ -1,4 +1,15 @@
 <script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Checkbox from '@/Components/Checkbox.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import InputError from '@/Components/InputError.vue';
+import Modal from '@/Components/Modal.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
@@ -7,6 +18,5 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Пост" />
 
     <AuthenticatedLayout>
-        
     </AuthenticatedLayout>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
+import NavLink from '@/Components/NavLink.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Head } from '@inertiajs/vue3';
 
 </script>
 
 <template>
-    <p>Добре дошли в TopOffers България.</p>
-    
-    <div class="section__links">
-        <Link href="/login">Login</Link>
-        <Link href="/register">Register</Link>
-    </div>
+    <GuestLayout>
+        <Head title="Добре дошли" />
+
+        <p>Добре дошли в TopOffers България.</p>
+        
+        <div class="section__links">
+            <NavLink href="/login">Login</NavLink>
+            <NavLink href="/register">Register</NavLink>
+        </div>
+    </GuestLayout>
 </template>
 
 <style scoped lang="scss">
