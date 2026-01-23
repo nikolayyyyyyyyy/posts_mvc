a8233bc68f9c8f560f239ede09fcaf23e6d14630
Николай <149511212+nikolayyyyyyyyy@users.noreply.github.com>
2026-01-23 21:34:07 +0200

    adding welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+
+</script>
+
 <template>
+    <p>Hello to our app</p>
     
+    <div class="section__links">
+        <Link href="/login">Login</Link>
+        <Link href="/register">Register</Link>
+    </div>
 </template>
+
+<style scoped>
+.section__links{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+</style>
